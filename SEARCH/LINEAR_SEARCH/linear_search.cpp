6af48e10c8d450ad078bcfc2997e96656e9addee
G. Radha Krishna <30743227+radhakrishnamurthy@users.noreply.github.com>
2020-04-14 09:30:41 +0530

    Update linear_search.cpp

--- SEARCH/LINEAR_SEARCH/linear_search.cpp
+++ SEARCH/LINEAR_SEARCH/linear_search.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout<<"HELLO";
+	cout<<"Enter the size of array";
+	int n;
+	cin>>n;
+	int arr = new int[n];
+	cout<<"Enter array elements:";
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	cout<<"Enter the element to be searched: ";
+	int k;
+	cin>>k;
+	for(int i=0;i<n;i++){
+		if(arr[i]==k){
+			cout<<"Element found at index "<<i;
+			return 0;
+		}
+	}
+	cout<<"Element not found!";
 return 0;
 }
