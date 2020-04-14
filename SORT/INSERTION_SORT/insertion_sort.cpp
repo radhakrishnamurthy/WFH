c039dc234c179475f1bfd700b865ffcf2807406a
G. Radha Krishna <30743227+radhakrishnamurthy@users.noreply.github.com>
2020-04-14 10:42:57 +0530

    Add files via upload

---
+++ SORT/INSERTION_SORT/insertion_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std; 
+void insertionSort(int arr[], int n)  
+{  
+    int i, key, j;  
+    for (i=1;i<n;i++) 
+    {  
+        key=arr[i];  
+        j=i - 1;  
+        while(j >= 0 && arr[j] > key) 
+        {  
+            arr[j+1]=arr[j];  
+            j=j-1;  
+        }  
+        arr[j+1]=key;  
+    }  
+}  
+
+int main()  
+{  
+    int n,result;
+	cout<<"Enter the sie of array: ";
+	cin>>n;
+	int* arr = new int[n];
+	cout<<"Enter the array elements: ";
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+    insertionSort(arr, n);  
+    for (int i = 0; i < n; i++)  
+        cout << arr[i] << " ";  
+  
+    return 0;  
+}
